5c0ee7801ab622fb73e6df1baf23b0f2ed845b36
jugal2 <parmarjugal2@gmail.com>
2022-07-21 11:46:33 +0530

    test

--- lib/Screens/TestingModules/InfiniteScroll.dart
+++ lib/Screens/TestingModules/InfiniteScroll.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:vivii/globals.dart' as global;
 
@@ -96,12 +97,102 @@ class _FutureInfiniteScrollState extends State<FutureInfiniteScroll> {
                 }
                 return true;
               },
-              child: ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  return card(
-                    data[index]['product_id'],
-                    data[index]['product_name'],
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio: 0.59, crossAxisCount: 2),
+                scrollDirection: Axis.vertical,
+                itemCount: data == null ? 0 : data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    semanticContainer: true,
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    color: Colors.transparent,
+                    elevation: 0,
+                    child: Column(
+                      children: [
+                        Container(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.network(
+                              data[index]['image_path'],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 5),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Flexible(
+                              child: Text(
+                                data[index]['product_name'],
+                                style: GoogleFonts.nunito(fontSize: 11),
+                                maxLines: 2,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              ' \u{20B9}' + " " + data[index]['sell_price'],
+                              style: GoogleFonts.nunitoSans(
+                                  fontWeight: FontWeight.bold, fontSize: 14),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 5),
+                            ),
+                            Text(
+                              ' \u{20B9}' + " " + data[index]['price'],
+                              style: GoogleFonts.nunitoSans(
+                                fontSize: 10,
+                                textStyle: TextStyle(
+                                  decoration: TextDecoration.lineThrough,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 5),
+                            ),
+                            Text(
+                              " " +
+                                  data[index]['discount_percentage']
+                                      .toString() +
+                                  "% " +
+                                  "off",
+                              style: GoogleFonts.nunitoSans(
+                                  fontWeight: FontWeight.bold, fontSize: 10),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              data[index]['offer_price'],
+                              style: GoogleFonts.nunito(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Selling Fast",
+                              style: GoogleFonts.nunito(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   );
                 },
               ),
